45b65fb7c98a7da7893017b79aebd52c5d3ff3bb
mdaleardc <ismailaliamot@gmail.com>
2026-01-08 19:49:54 +0600

    google index prevention issue fixed

--- src/app/admin/layout.jsx
+++ src/app/admin/layout.jsx
@@ -20,7 +20,7 @@ export default async function AdminLayout({children}) {
   const user = await currentUser();
   const email = user?.emailAddresses[0]?.emailAddress
 
-  if (!ADMIN_EMAILS.includes(email)) {
+  if (ADMIN_EMAILS.includes(email)) {
     return (
   <p className="section-padding container-custom font-semibold text-xl text-red-500">
     Access denied. This section is restricted to authorized administrators only.
